4c0291da4d4f79a48c065fc5144d18ca619b9d20
Joshua Brunscheen <114787256+josh-brunscheen@users.noreply.github.com>
2024-11-09 15:22:08 -0500

    Map working :)

--- BikeChainProject/src/App.jsx
+++ BikeChainProject/src/App.jsx
@@ -1,7 +1,33 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    // This function will be called after the component renders
+    console.log('Component has rendered!');
+
+    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+      key: "APIKEYHERE",
+      v: "weekly",
+      // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+      // Add other bootstrap parameters as needed, using camel case.
+    });
+
+    let map;
+
+    async function initMap() {
+      const { Map } = await google.maps.importLibrary("maps");
+
+      map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+    }
+
+    initMap();
+  }, []); // The empty array ensures the effect runs only once, after the initial render
+
   return (
     <div className="text-black">
       <div id="logo" className="flex size- justify-center">
@@ -10,8 +36,7 @@ function App() {
       
       <div id="mainContent"className="flex justify-center text-black rounded">
         {/* Map div */}
-        <div className="w-2/3 m-4 border-2 border-black shadow-lg rounded-md">
-          Map
+        <div id="map" className="w-2/3 m-4 border-2 border-black shadow-lg rounded-md">
         </div>
 
         {/* Info panel here */}
